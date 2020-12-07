a6d69a34bb689d0d318a623a896ef5c9746eb861
Gonçalo Domingos <quartime02@gmail.com>
2020-12-07 17:03:16 +0000

    Mudanças_Definicoes_showEvent

--- app/src/main/java/pt/ubi/di/pdm/happeningubi/DefinicoesActivity.java
+++ app/src/main/java/pt/ubi/di/pdm/happeningubi/DefinicoesActivity.java
@@ -1,28 +1,88 @@
 package pt.ubi.di.pdm.happeningubi;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class DefinicoesActivity extends AppCompatActivity {
+public class DefinicoesActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView Idioma,Mudar_Foto,Reportar_Problema,Conectar_Google,Conectar_Facebook;
+
+    private EditText Assunto_Problema,Descricao_Problema;
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private Button Enviar,Cancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_definicoes);
 
-        Idioma = (TextView) findViewById(R.layout.mudar_idioma_definicoes_textview);
-        Mudar_Foto = (TextView) findViewById(R.layout.mudar_foto_definicoes_textview);
-        Reportar_Problema = (TextView) findViewById(R.layout.reportar_problema_definicoes_textview);
-        Conectar_Facebook = (TextView) findViewById(R.layout.conectar_facebook_definicoes_textview);
-        Conectar_Google = (TextView) findViewById(R.layout.conectar_google_definicoes_textview);
+        Idioma = (TextView) findViewById(R.id.mudar_idioma_definicoes_textview);
+        Mudar_Foto = (TextView) findViewById(R.id.mudar_foto_definicoes_textview);
+        Reportar_Problema = (TextView) findViewById(R.id.reportar_problema_definicoes_textview);
+        Conectar_Facebook = (TextView) findViewById(R.id.conectar_facebook_definicoes_textview);
+        Conectar_Google = (TextView) findViewById(R.id.conectar_google_definicoes_textview);
 
 
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+
+            case R.id.mudar_foto_definicoes_textview:
+
+                break;
+            case R.id.reportar_problema_definicoes_textview: //popup com "assunto" e "mensagem"
+                createReport();
+                break;
+            case R.id.mudar_idioma_definicoes_textview: // mudar as strings
+
+                break;
+            case R.id.conectar_facebook_definicoes_textview:
+
+                break;
+            case R.id.conectar_google_definicoes_textview:
+
+                break;
+        }
+
+    }
+    private void createReport(){
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View ReportPopupView = getLayoutInflater().inflate(R.layout.popup_reportproblem,null);
+        Assunto_Problema = (EditText) ReportPopupView.findViewById(R.id.assunto_report_edittext);
+        Descricao_Problema = (EditText) ReportPopupView.findViewById(R.id.descricao_report_edittext);
+        Enviar = (Button) ReportPopupView.findViewById(R.id.enviar_report_button);
+        Cancelar = (Button) ReportPopupView.findViewById(R.id.cancelar_report_button);
+
+        dialogBuilder.setView(ReportPopupView);
+        dialog = dialogBuilder.create();
+        dialog.show();
+        Enviar.setOnClickListener(v -> {
+
+            String Assunto,Descricao,Utilizador;
+            Assunto = Assunto_Problema.getText().toString();
+            Descricao = Descricao_Problema.getText().toString();
+
+            //Utilizador???
+
+            //inserir base de dados
+
+            Toast.makeText(DefinicoesActivity.this,"Enviado com sucesso",Toast.LENGTH_SHORT).show(); //em caso de sucesso
+
+        });
+        Cancelar.setOnClickListener(v -> dialog.dismiss());
     }
 }
--- app/src/main/java/pt/ubi/di/pdm/happeningubi/ShowEventActivity.java
+++ app/src/main/java/pt/ubi/di/pdm/happeningubi/ShowEventActivity.java
@@ -1,14 +1,51 @@
 package pt.ubi.di.pdm.happeningubi;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ShowEventActivity extends AppCompatActivity {
+
+    private ImageView edit_descricao;
+    private AlertDialog.Builder dialogbuilder;
+    private AlertDialog dialog;
+    private TextView Descricao;
+    private EditText NewDescricao;
+    private Button Cancelar,Editar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_event);
+
+        Descricao = (TextView) findViewById(R.id.descricao_evento_textview);
+        edit_descricao = (ImageView) findViewById(R.id.edit_descricao_imageview);
+
+        edit_descricao.setOnClickListener(v -> createPopup());
+    }
+    private void createPopup(){
+        dialogbuilder = new AlertDialog.Builder(this);
+        final View EditDescPopupView = getLayoutInflater().inflate(R.layout.popup_editar_descricao_evento,null);
+        NewDescricao = (EditText) EditDescPopupView.findViewById(R.id.nova_descricao_editText);
+        Editar = (Button) EditDescPopupView.findViewById(R.id.editar_descricao_popup_button);
+        Cancelar = (Button) EditDescPopupView.findViewById(R.id.cancelar_editar_descricao_popup_button);
+
+        Editar.setOnClickListener(v -> {
+            String nova_descricao = NewDescricao.getText().toString().trim();
+            //Inserir na base de dados
+
+            Toast.makeText(ShowEventActivity.this,"Enviado com sucesso",Toast.LENGTH_SHORT).show();
+        });
+        Cancelar.setOnClickListener(v -> dialog.dismiss());
+
     }
 }
