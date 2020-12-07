d40d089d544a1e3dba3f057fde6c7dd849dd968b
rubenguilherme <ruben.henriques.guilherme@gmail.com>
2020-12-07 10:39:56 +0000

    Corrigidos alguns bugs que permitem que a app corra em API 21.

--- app/src/main/java/pt/ubi/di/pdm/happeningubi/DefinicoesActivity.java
+++ app/src/main/java/pt/ubi/di/pdm/happeningubi/DefinicoesActivity.java
@@ -1,12 +1,9 @@
 package pt.ubi.di.pdm.happeningubi;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.os.Bundle;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 
 public class DefinicoesActivity extends AppCompatActivity {
 
@@ -17,11 +14,11 @@ public class DefinicoesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_definicoes);
 
-        Idioma = (TextView) findViewById(R.layout.mudar_idioma_definicoes_textview);
-        Mudar_Foto = (TextView) findViewById(R.layout.mudar_foto_definicoes_textview);
-        Reportar_Problema = (TextView) findViewById(R.layout.reportar_problema_definicoes_textview);
-        Conectar_Facebook = (TextView) findViewById(R.layout.conectar_facebook_definicoes_textview);
-        Conectar_Google = (TextView) findViewById(R.layout.conectar_google_definicoes_textview);
+        Idioma = (TextView) findViewById(R.id.mudar_idioma_definicoes_textview);
+        Mudar_Foto = (TextView) findViewById(R.id.mudar_foto_definicoes_textview);
+        Reportar_Problema = (TextView) findViewById(R.id.reportar_problema_definicoes_textview);
+        Conectar_Facebook = (TextView) findViewById(R.id.conectar_facebook_definicoes_textview);
+        Conectar_Google = (TextView) findViewById(R.id.conectar_google_definicoes_textview);
 
 
     }
