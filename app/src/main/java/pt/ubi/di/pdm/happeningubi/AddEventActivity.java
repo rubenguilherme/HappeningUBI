f075eebcb5eca2cd0bb4659cf698d9e2218c5c4b
p_duarte <paulo.duarte@ubi.pt>
2020-12-12 15:24:02 +0000

    AddEventActivity Pronto

--- app/src/main/java/pt/ubi/di/pdm/happeningubi/AddEventActivity.java
+++ app/src/main/java/pt/ubi/di/pdm/happeningubi/AddEventActivity.java
@@ -4,12 +4,19 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 
+import android.Manifest;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
@@ -17,15 +24,46 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-import java.util.Date;
-import java.util.Locale;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-public class AddEventActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class AddEventActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+
+    public static int id = 1;
+    public static int img_id = 1;
 
     EditText editTName, editTDesc, editTLoc;
+    ImageView ivImage;
     DatePicker datePicker;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageRef = storage.getReference();
+    int hour, minute;
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
+    ArrayList<Uri> files = new ArrayList<>();
+    ArrayList<Integer> id_files = new ArrayList<>();
+    boolean existFile = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +77,9 @@ public class AddEventActivity extends AppCompatActivity {
         editTDesc = findViewById(R.id.addevent_desc);
         editTLoc = findViewById(R.id.addevent_loc);
         datePicker = findViewById(R.id.addevent_date);
+        //ivImage = findViewById(R.id.ivImage);
         //setAppLocale("applanguage"); Gonçalo -> Mudar Idioma -> NAO APAGAR
+
     }
 
     @Override
@@ -64,10 +104,42 @@ public class AddEventActivity extends AppCompatActivity {
     }
 
     public void eventAdded() {
-        EventClass e = new EventClass(editTName.getText().toString(),editTDesc.getText().toString(), editTLoc.getText().toString(),"USER100" , null, new Date(datePicker.getYear(),datePicker.getMonth(),datePicker.getDayOfMonth()),-1);
-        Intent result = new Intent();
-        result.putExtra("event", e);
-        setResult(RESULT_OK, result);
+        Map<String, Object> event = new HashMap<>();
+        event.put("name", editTName.getText().toString());
+        event.put("description", editTDesc.getText().toString());
+        event.put("location", editTLoc.getText().toString());
+        //Calendar calendar = new GregorianCalendar(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth(), hour, minute);
+        event.put("event_date", new Timestamp(new Date((datePicker.getYear() - 1900), datePicker.getMonth(), datePicker.getDayOfMonth(), hour, minute)));
+        event.put("id", id);
+        event.put("user_id", -1);
+        event.put("images", id_files);
+        db.collection("Event")
+                .add(event)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("TAG", "Error adding document", e);
+                    }
+                });
+        if (existFile) {
+            int size = 1;
+            while (size <= files.size()) {
+                StorageReference imagesRef = storageRef.child("images/" + id_files.get(size - 1) + ".jpg");
+                UploadTask uploadTask = imagesRef.putFile(files.get(size - 1));
+                size++;
+            }
+        }
+        id++;
+        //EventClass e = new EventClass(editTName.getText().toString(),editTDesc.getText().toString(), editTLoc.getText().toString(),"USER100" , null, new Date(datePicker.getYear(),datePicker.getMonth(),datePicker.getDayOfMonth()),-1);
+        //Intent result = new Intent();
+        //result.putExtra("event", e);
+        //setResult(RESULT_OK, result);
         super.finish();
     }
 
@@ -77,7 +149,40 @@ public class AddEventActivity extends AppCompatActivity {
     }
 
     public void toDo(View view) {
-        Toast.makeText(this, "Funcionalidade ainda não implementada.", Toast.LENGTH_SHORT).show();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                requestPermissions(permissions, PERMISSION_CODE);
+            }
+            else {
+                pickImageFromGallery();
+            }
+        }
+        else {
+
+        }
+    }
+
+    private void pickImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            files.add(data.getData());
+            existFile = true;
+            id_files.add(img_id);
+            img_id++;
+            //ivImage.setImageURI(img);
+        }
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
     }
 
     private void setAppLocale(String localeCode){ //Gonçalo -> Mudar Idioma -> NAO APAGAR
@@ -86,5 +191,24 @@ public class AddEventActivity extends AppCompatActivity {
         Configuration conf = res.getConfiguration();
         conf.setLocale(new Locale(localeCode.toLowerCase()));
         res.updateConfiguration(conf,dm);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        hour = hourOfDay;
+        this.minute = minute;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImageFromGallery();
+                }
+                else {
+                    Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show();
+                }
+        }
     }
 }
