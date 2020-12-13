53a290f09106b693e5f67647d31486aa20972960
p_duarte <paulo.duarte@ubi.pt>
2020-12-13 19:49:25 +0000

    RegisterActivity and EditProfileActivity added functions related with upload of photos.

--- app/src/main/java/pt/ubi/di/pdm/happeningubi/EditProfileActivity.java
+++ app/src/main/java/pt/ubi/di/pdm/happeningubi/EditProfileActivity.java
@@ -2,12 +2,18 @@ package pt.ubi.di.pdm.happeningubi;
 
 import androidx.annotation.NonNull;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -15,6 +21,9 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class EditProfileActivity extends Util {
 
@@ -23,6 +32,10 @@ public class EditProfileActivity extends Util {
     private EditText name;
     private TextView email;
     private String pass, docID, emailS;
+    Uri photo;
+    boolean existFile = false;
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     
@@ -74,7 +87,12 @@ public class EditProfileActivity extends Util {
     public void confirmChanges(View v){
 
         db.collection("users").document(docID).update("name", String.valueOf(name.getText()));
-
+        if (existFile) {
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference();
+            StorageReference imagesRef = storageRef.child("userimages/" + readUser() + ".jpg");
+            UploadTask uploadTask = imagesRef.putFile(photo);
+        }
         finish();
     }
 
@@ -102,5 +120,43 @@ public class EditProfileActivity extends Util {
 
     }
 
+    public void changePic(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                requestPermissions(permissions, PERMISSION_CODE);
+            }
+            else {
+                pickImageFromGallery();
+            }
+        }
+        else {}
+    }
 
+    public void pickImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            photo = data.getData();
+            existFile = true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImageFromGallery();
+                }
+                else {
+                    Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
 }
