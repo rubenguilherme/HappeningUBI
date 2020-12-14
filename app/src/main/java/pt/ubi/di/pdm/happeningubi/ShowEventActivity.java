e1f89ff47203a72109549396a36c3485da59eea7
Gonçalo Domingos <quartime02@gmail.com>
2020-12-14 16:04:23 +0000

    Small stuff

--- app/src/main/java/pt/ubi/di/pdm/happeningubi/ShowEventActivity.java
+++ app/src/main/java/pt/ubi/di/pdm/happeningubi/ShowEventActivity.java
@@ -27,19 +27,14 @@ import java.util.Calendar;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-public class ShowEventActivity extends Util implements View.OnClickListener {
+public class ShowEventActivity extends Util {
 
-    private ImageView edit_descricao,imagem_evento,back_arrow,front_arrow,showevent_Back;
+    private ImageView imagem_evento,showevent_Back;
     private AlertDialog.Builder dialogbuilder;
     private AlertDialog dialog;
     private TextView Descricao,Titulo,Date,Horas;
-    private EditText NewDescricao;
-    private Button Cancelar,Editar;
     private StorageReference storageRef;
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
     private static final String TAG = "ShowEventActivity";
-
-    private static int i = 0;
 
     private String descricao,titulo,localizacao,horas,time_stamp_data;
     private ArrayList<Long> imagens;
@@ -69,17 +64,7 @@ public class ShowEventActivity extends Util implements View.OnClickListener {
         Date = (TextView) findViewById(R.id.date_evento_textview);
         Horas = (TextView) findViewById(R.id.horas_evento_textview);
         imagem_evento = (ImageView) findViewById(R.id.imagem_evento_imageview);
-        back_arrow = (ImageView) findViewById(R.id.imagem_anterior_imageview);
-        front_arrow = (ImageView) findViewById(R.id.imagem_aseguir_imageview);
-        showevent_Back = (ImageView) findViewById(R.id.showevent_back);
 
-        showevent_Back.setOnClickListener(v -> {
-            i = 0;
-            finish();
-        });
-
-        back_arrow.setOnClickListener(this);
-        front_arrow.setOnClickListener(this);
 
         Titulo.setText(titulo);
         Descricao.setText(descricao);
@@ -88,41 +73,10 @@ public class ShowEventActivity extends Util implements View.OnClickListener {
 
         //clicar next and before to change event image
         if (event.getImages().size() > 0) {
-            GlideApp.with(this).load(storageRef.child("images/" + event.getImages().get(i) + ".jpg")).into(imagem_evento);
+            GlideApp.with(this).load(storageRef.child("images/" + event.getImages().get(0) + ".jpg")).into(imagem_evento);
         }
         //
 
-    }
-    @Override
-    public void onClick(View v) {
-        if (event.getImages().size() > 0) {
-            switch (v.getId()) {
-                case R.id.imagem_anterior_imageview:
-                    if ((i-1) < 0){
-                        Log.d(TAG, "-1 imagem :" + i);
-                        i = (event.getImages().size()) - 1;}
-                    else i--;
-                    GlideApp.with(this).load(storageRef.child("images/" + event.getImages().get(i) + ".jpg")).into(imagem_evento);
-                    Log.d(TAG, "new imagem :" + i);
-                    break;
-                case R.id.imagem_aseguir_imageview:
-                    if ((i+1) == event.getImages().size()){
-                        Log.d(TAG, "+1 imagem :" + i);
-                        i = 0;}
-                    else i++;
-                    GlideApp.with(this).load(storageRef.child("images/" + event.getImages().get(i) + ".jpg")).into(imagem_evento);
-                    Log.d(TAG, "new imagem :" + i);
-                    break;
-            }
-        }
-    }
-
-    private void restart(){
-        Intent intent = new Intent(ShowEventActivity.this,ShowEventActivity.class);
-        finish();
-        overridePendingTransition(0,0);
-        startActivity(intent);
-        overridePendingTransition(0,0);
     }
     private void getEventDetails(EventClass event){
         titulo = event.getName();
